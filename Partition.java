560f064b1376fca89cc595aebf07b7f91830e2b1
CatrerMarkegard <markegar@grinnell.edu>
2018-10-17 22:02:04 -0500

    worked on select

--- Partition.java
+++ Partition.java
@@ -2,7 +2,7 @@ package pictorialLoop;
 
 public class Partition {
 
-	public static void partition (int a[], int left, int right) {
+	public static int partition (int a[], int left, int right) {
 
 
 		int l_spot = 1;
@@ -11,60 +11,81 @@ public class Partition {
 
 		while (l_spot <= r_spot) {
 
-			while ((a[l_spot]<=a[left]) && (a[r_spot]>=a[l_spot])) {
+			while ((a[l_spot]<=a[left]) && (r_spot>=l_spot)) {
 				l_spot++;
 			}
 
-			while ((a[r_spot]>=a[left]) && (a[l_spot]<=a[r_spot])) {
+			while ((a[r_spot]>=a[left]) && (l_spot<=r_spot)) {
 				r_spot--;
 			}
-			if (a[r_spot]>a[l_spot]) {
+			if (r_spot>l_spot) {
 				temp = a[l_spot];
 				a[l_spot] = a[r_spot];
 				a[r_spot] = temp;
-//				if(l_spot!=r_spot +1) {
-//					l_spot++;
-//					r_spot--;
-//				}
 			}
 
-			//			if (a[r_spot] >= a[left]) {
-			//				r_spot--;
-			//			}
-			//			if (a[l_spot] <= a[left]) {
-			//				l_spot++;
-			//			}
-			//			if (a[l_spot]>a[left] && a[r_spot]<a[left]){ 
-			//				temp = a[l_spot];
-			//				a[l_spot] = a[r_spot];
-			//				a[r_spot] = temp;
-			//				l_spot++;
-			//				r_spot--;
-			//			}
-			//		}
-			//		temp = a[r_spot];
-			//		a[r_spot] = a[left];
-			//		a[left] = temp;
-
 		}
-		System.out.println();
-		for (int i = 0; i<a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
-		//swap l_spot and r_spot
-		//		temp = a[l_spot];
-		//		a[l_spot] = a[r_spot];
-		//		a[r_spot] = temp;
-		//		System.out.println();
-		//		for (int i = 0; i<a.length; i++) {
-		//			System.out.print(a[i] + " ");
-		//		}
 
 		//swap first element with the middle element
 		temp = a[r_spot];
 		a[r_spot] = a[left];
 		a[left] = temp;
 
+		return r_spot;
+	}
+	//
+	//	public static int select (int a[], int n, int k) {
+	//		if(n==1) {
+	//			return a[0];
+	//		}
+	//		int middle = partition(a, 0 , n - 1);//
+	//		for (int i = 0; i<n; i++) {
+	//			System.out.print(a[i] + " ");
+	//		}
+	//		System.out.println("before");	
+	//		System.out.println(middle + " K: "+ k); 
+	//		System.out.println(); 
+	//		
+	//		if(k == middle + 1) {
+	//			return a[middle];
+	//		}
+	//		
+	//		if(k <= middle) {
+	////			for (int i = 0; i<n; i++) {
+	////				System.out.print(a[i] + " ");
+	////			}
+	////			System.out.println();
+	//			
+	//			select(a, middle, k);
+	//		}
+	//		
+	//		if(k >= middle) {
+	//			for (int i = 0; i<n; i++) {
+	//				System.out.print(a[i] + " ");
+	//			}
+	//			System.out.println();
+	//			select(a, middle + 1, k);
+	//		}
+	//		return -1; //added because the syntax requires the return statement s
+	//	}
+
+
+	public static int select (int a[], int k) {
+		return selectHelper (a, k, 0, a.length-1);
+	}
+
+	public static int selectHelper (int a[], int k, int left, int right) {
+		int s = -1;
+		int middle = partition(a,left,a.length-1);
+		if(middle == k - 1) {
+			return a[middle];
+		}
+		else if(middle < k - 1) {
+			return selectHelper(a,k - middle,middle+1, right);
+		}
+		else  {
+			return selectHelper(a,k,0,middle - 1);
+		}
 	}
 
 
@@ -74,20 +95,31 @@ public class Partition {
 		int [] c = {35,10,71,30,50,8}; //odd number of elements
 		int l = 0;
 		int r = b.length - 1;
+		int p =0;
+		int s = -1;
+
+		//p = partition(b,l,r);
+		s = select (b,2);
+
+		System.out.print(s+ "   ");
+//		for (int i = 0; i<b.length; i++) {
+//			System.out.print(b[i] + " ");
+//		}
+//		System.out.println();
+		s = select(c,5);
+		System.out.print(s+ "    ");
+//
+//		for (int i = 0; i<c.length; i++) {
+//			System.out.print(c[i] + " ");
+//		}
 
 
-		partition(b,l,r);
-		System.out.println();
-		for (int i = 0; i<b.length; i++) {
-			System.out.print(b[i] + " ");
-		}
-		System.out.println();
-		partition(c,l,(c.length -1));
-		//		for (int i = 0; i<c.length; i++) {
-		//			System.out.print(c[i] + " ");
-		//		}
 
 
 
 	}
+
+
+
 }
+
